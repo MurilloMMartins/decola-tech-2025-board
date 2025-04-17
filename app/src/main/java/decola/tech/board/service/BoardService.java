2e770a4ae915ce35c70c7999d531ec4a79b2ccdb
MurilloMMartins <murillomartins19@gmail.com>
2025-04-17 16:01:04 -0300

    Started to make the board service

---
+++ app/src/main/java/decola/tech/board/service/BoardService.java
@@ -0,0 +1,30 @@
+package decola.tech.board.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import decola.tech.board.persistence.dao.BoardDAO;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BoardService {
+
+    private final Connection connection;
+
+    public boolean delete(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        try {
+            if (!dao.exists(id)) {
+                return false;
+            }
+
+            dao.delete(id);
+            connection.commit();
+
+            return true;
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+    }
+}
