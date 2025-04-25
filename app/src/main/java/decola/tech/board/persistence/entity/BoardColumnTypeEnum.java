02cc5a2030e8fc86a4f6f24dca181e3112e96ed6
MurilloMMartins <murillomartins19@gmail.com>
2025-04-25 15:40:40 -0300

    Added functionality to find enum type by name

--- app/src/main/java/decola/tech/board/persistence/entity/BoardColumnTypeEnum.java
+++ app/src/main/java/decola/tech/board/persistence/entity/BoardColumnTypeEnum.java
@@ -1,7 +1,15 @@
 package decola.tech.board.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnTypeEnum {
 
-    TODO, INPROGRESS, COMPLETED, CANCELED
+    TODO, INPROGRESS, COMPLETED, CANCELED;
+        
+    public static BoardColumnTypeEnum findByName(final String name) {
+        return Stream.of(BoardColumnTypeEnum.values())
+                .filter(b -> b.name().equals(name))
+                .findFirst().orElseThrow();
+    }
 
 }
