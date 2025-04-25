61d5281d7c1111df722fd3e3f11c981f5b714ddc
MurilloMMartins <murillomartins19@gmail.com>
2025-04-25 15:40:07 -0300

    Finished coding 'findByBoardId' functionality

--- app/src/main/java/decola/tech/board/persistence/dao/BoardColumnDAO.java
+++ app/src/main/java/decola/tech/board/persistence/dao/BoardColumnDAO.java
@@ -1,7 +1,10 @@
 package decola.tech.board.persistence.dao;
 
+import static decola.tech.board.persistence.entity.BoardColumnTypeEnum.findByName;
+
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mysql.cj.jdbc.StatementImpl;
@@ -23,7 +26,7 @@ public class BoardColumnDAO {
             statement.setString(i++, entity.getType().name());
             statement.setLong(i, entity.getBoard().getId());
             statement.executeUpdate();
-            
+
             if (statement instanceof StatementImpl impl) {
                 entity.setId(impl.getLastInsertID());
             }
@@ -33,7 +36,22 @@ public class BoardColumnDAO {
     }
 
     public List<BoardColumnEntity> findByBoardId(Long id) throws SQLException {
-        // TODO Auto-generated method stub
+        List<BoardColumnEntity> entities = new ArrayList<>();
+        var sql = "SELECT id, name, `order` FROM boards_columns WHERE board_id = ? ORDER BY `order`";
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setType(findByName(resultSet.getString("type")));
+                entities.add(entity);
+            }
+
+        }
         throw new UnsupportedOperationException("Unimplemented method 'findByBoardId'");
     }
 }
